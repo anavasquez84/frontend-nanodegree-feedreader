6560c64dd7c0f08a1b5c3aa9f6713288092da246
Ana <anavasquez@Anas-MacBook-Air.local>
2015-11-03 18:28:52 -0500

    Feat: write RSS test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,19 +26,28 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         if('feed URLs are defined', function() {
+            for(var i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url).not.toBe('');
+            }
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         if('feed names are defined', function() {
+            for(var i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name).not.toBe('');
+            }
+         });
     });
-
 
     /* TODO: Write a new test suite named "The menu" */
 
